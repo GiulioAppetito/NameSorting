7bb7441f0a690438397d9ac3255ffff84be3eb5e
Giulio <giulio.appetito.ga@gmail.com>
2022-02-17 11:23:45 +0100

    Initial commit

---
+++ src/NameSorter.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class NameSorter {
+    public static void main(String[] args)
+    {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Giulio");
+        names.add("Anastasia");
+        names.add("Matteo");
+        names.add("Alessandro");
+        names.add("Sara");
+        names.add("Gabriele");
+
+        Collections.sort(names);
+
+        System.out.println("List after the use of" +
+                " Collection.sort() :\n" + names);
+    }
+}
